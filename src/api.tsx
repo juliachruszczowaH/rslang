7f4e9e01afcc019473e5fd71a294d32546d31b54
Alex <olivesargreek@gmail.com>
2022-02-04 03:09:54 +0300

    create: api.tsx

---
+++ src/api.tsx
@@ -0,0 +1,18 @@
+import { API_URL } from "constants/constants";
+
+// получение всех категорий
+
+// const getAllCategories = async () => {
+//   const response = await fetch(API_URL + 'categories.php');
+//   return await response.json();
+// };
+
+// получение  отдельной категории
+
+// const getFilteredCategory = async (catName) => {
+//   const response = await fetch(API_URL + 'filter.php?c=' + catName);
+//   return await response.json();
+
+// };
+
+  // export { getAllCategories, getFilteredCategory }
