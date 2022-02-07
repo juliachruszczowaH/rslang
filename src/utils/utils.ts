57f645b975872a0d05c2481c3ab6109aef9ace62
novikova.n_qschool <nadeysha23@gmail.com>
2022-02-07 15:58:48 +0400

    feat: add helper functions

---
+++ src/utils/utils.ts
@@ -0,0 +1,9 @@
+export const shuffleArray = (array: any[])=>
+{
+  return array.map(i=>[Math.random(), i]).sort().map(i=>i[1])
+
+}
+export const getRandomNumber = (min: number, max: number) => {
+  return Math.floor(Math.random() * max);
+}
+
