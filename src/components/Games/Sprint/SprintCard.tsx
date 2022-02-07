c657d9383396c87d44ed4ef8ef0c3b380bcb1c9d
novikova.n_qschool <nadeysha23@gmail.com>
2022-02-07 15:53:59 +0400

    feat: add sprints  Q&A card

---
+++ src/components/Games/Sprint/SprintCard.tsx
@@ -0,0 +1,44 @@
+import React, { FC, useState, useCallback } from "react";
+import Button  from "../common/Button";
+import { getRandomNumber } from "../common/utils";
+import { AnswerObject } from "./SprintGameField";
+
+type Props = {
+  questionNumber: number;
+  questionsWord: string;
+  answers: string[];
+  /* posibleAnswerTranslation: string; */
+  onAnswer: (answerCompare: boolean) => void;
+  userAnswer: AnswerObject;
+  correctAnswer?: boolean;
+};
+const SprintCard: FC</*ISprintData  */ Props> = ({
+  questionsWord,
+  answers,
+  onAnswer,
+  /* userAnswer, */
+  correctAnswer,
+  questionNumber,
+}) => {
+
+  const randomAnswer = getRandomNumber(0, answers.length - 1);
+  
+  return (
+    <div>
+      <p>Question: {questionNumber}</p>
+      <p>{questionsWord}</p>
+      <div>
+        <div
+          /* disabled={userAnswer ? true : false} */
+          
+        >{answers[randomAnswer]}</div>
+      </div>
+      <div>
+      <Button /* value={answers[randomAnswer]}  */onClick={()=>{onAnswer(true)}}>True</Button>
+      <Button onClick={()=>{onAnswer(false)}}>False</Button>
+      </div>
+    </div>
+  );
+};
+
+export default SprintCard;
