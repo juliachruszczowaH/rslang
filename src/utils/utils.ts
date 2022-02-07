76478294abde316f9f6fcdf86f86a54966dd4069
novikova.n_qschool <nadeysha23@gmail.com>
2022-02-07 15:59:56 +0400

    fix: fix warning of eslint in utils

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,9 +1,10 @@
-export const shuffleArray = (array: any[])=>
-{
-  return array.map(i=>[Math.random(), i]).sort().map(i=>i[1])
-
-}
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const shuffleArray = (array: any[]) => {
+  return array
+    .map((i) => [Math.random(), i])
+    .sort()
+    .map((i) => i[1]);
+};
 export const getRandomNumber = (min: number, max: number) => {
   return Math.floor(Math.random() * max);
-}
-
+};
