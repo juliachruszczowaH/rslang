ae4bea4b9f807fc1b7d82138147745bd6c1fb6a1
Alex <olivesargreek@gmail.com>
2022-02-11 13:32:13 +0300

    add: button playAudio

--- src/components/WordCard/WordCard.tsx
+++ src/components/WordCard/WordCard.tsx
@@ -2,7 +2,16 @@ import { IWordData } from '../../models/WordModel';
 import { API_URL } from '../../services/AppService';
 import './wordcard.css';
 
+
 export const WordCard = (word: IWordData) => {
+
+  function playAudio(){
+
+    const audio = new Audio( API_URL );
+    audio.play();
+
+  }
+
   return (
     <div className="card">
       <img
@@ -13,6 +22,9 @@ export const WordCard = (word: IWordData) => {
       ></img>
       <div className="card-body">
         <p className="card-text">{word.word}</p>
+        <button onClick={ playAudio }>Add Me
+
+        </button>
       </div>
     </div>
   );
