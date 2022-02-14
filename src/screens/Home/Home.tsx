36613ac33584a4daa9b6a8c06a2d7cbfbbe4d5e8
Alex <olivesargreek@gmail.com>
2022-02-14 09:24:25 +0300

    feat: basic Home page structure

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,14 +1,32 @@
 import React from 'react';
 import './home.css';
+import HomeCardItem from './HomeItem';
 
 const Home: React.FC = () => {
   return (
-    <div className="home-container">
-      <div className="home-content">
-        <h3>Home</h3>
+     
+    <div className="home-content">
+      <div>
+        <h1>ABOUT</h1>
+        <p>Learning English has never been so easy
+          Memorizing English words can be fun and challenging. Play games, listen to pronunciation, improve your knowledge. With our app, learning is a joy.
+        </p>
       </div>
+        <h3>Advantages</h3>
+        <div className='adventures'>
+        
+        </div>
+        <div className='adventures'></div> 
     </div>
+
+
   );
 };
 
 export default Home;
+
+
+
+
+
+
