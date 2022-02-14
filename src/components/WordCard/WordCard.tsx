854709929c4e3b689e9e8b083cbeb94f63b2a62b
Alex <olivesargreek@gmail.com>
2022-02-14 08:26:54 +0300

    feat: logic playCallback function

--- src/components/WordCard/WordCard.tsx
+++ src/components/WordCard/WordCard.tsx
@@ -1,15 +1,41 @@
+import { useCallback, useState, useEffect } from 'react';
+
 import { Button, Card, Icon, Image } from 'semantic-ui-react';
 import { IWordData } from '../../models/WordModel';
 import { API_URL } from '../../services/AppService';
+import { isAuthenticated } from '../../services/AuthService';
 import { play } from '../../utils/utils';
 import './wordcard.css';
 
-export const WordCard = (word: IWordData) => {
+export const WordCard = (word: IWordData, color?: string) => {
+  const [wordCounter, setWordCounter] = useState(0);
+  const [playList, setPlayList] = useState([word.audio, word.audioExample, word.audioMeaning]);
+  const [audio, setAudio] = useState(new Audio(word.audio));
+
+  const playCallback = useCallback(() => {
+    audio.play();
+  }, [audio]);
+
+
+  useEffect(() => {
+    if (playList[wordCounter]) {
+      audio.src = playList[wordCounter];
+      audio.play();
+    }
+  }, [wordCounter, audio, playList]);
+
+  audio.onended = function () {
+    setWordCounter(wordCounter + 1);
+  };
+
   return (
-    <Card>
-      <Image src={API_URL + word.image} raised ui={false} />
+    <Card raised>
+
+      <Image src={API_URL + word.image} ui={false} />
       <Card.Content>
-        <Card.Header>{word.word}</Card.Header>
+        <Card.Header as="h3" style={{ backgroundColor: color }}>        <Button onClick={ playCallback }>
+          <Icon name='headphones' />
+        </Button>{word.word}</Card.Header>
         <Card.Meta>
           <span className='transcription'>{word.transcription}</span>
         </Card.Meta>
@@ -17,31 +43,15 @@ export const WordCard = (word: IWordData) => {
           {word.wordTranslate}
         </Card.Description>
       </Card.Content>
-      <Card.Content>
-        <Card.Description>
-          {word.textExample}
-        </Card.Description>
-        <Card.Description>
-          {word.transcription}
-        </Card.Description>
-        <Card.Description>
-          {word.textMeaningTranslate}
-        </Card.Description>
-        <Card.Description>
-          {word.textExampleTranslate}
-        </Card.Description>
-        <Card.Description>
-          {word.textMeaning}
-        </Card.Description>
-      </Card.Content>
-      <Card.Content extra>
-        <Button onClick={() => play(API_URL + word.audioMeaning)}>
-          <Icon name='headphones' />
+      {isAuthenticated() ? (<Card.Content extra>
+        <Button onClick={() => console.log('difficult')}>
+          <Icon name='eye' />
         </Button>
-        <Button>
-          <Icon name='arrow alternate circle down' />
+        <Button onClick={() => console.log('known')}>
+          <Icon name='check circle outline' />
         </Button>
-      </Card.Content>
+      </Card.Content>) : null}
+
     </Card>
   );
 };
