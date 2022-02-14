0c729909107081667c3f38b3c9b130e11fa195bd
Alex <olivesargreek@gmail.com>
2022-02-14 17:37:53 +0300

    feat: basic Home page structure is done

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,19 @@
 import './footer.css';
+import github from '../../assets/github.png';
+import rss from '../../assets/rss-logo-unit.svg';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-container">
-        <div className="app-footer-logo">
-          <span>Github Logo</span>
+      <div className="footer-box">
+        <div className="footer-box-item">
+          <a href="https://github.com/juliachruszczowaH/rslang"><img className="footer-logo" src={ github } alt="github-logo" /></a>
         </div>
-        <span>Footer APP</span>
-        <div className="rss-logo">
-          <span>RSS logo</span>
+        <div className="footer-box-item">
+           <p className="item-styles"> RSS Q3-2021</p>
+        </div>
+        <div className="footer-box-item">
+          <a href="https://rs.school/js/"><img className="footer-logo" src={ rss } alt="rss-logo" /></a>
         </div>
       </div>
     </footer>
