f656ad98e237a8c74136b08fac08bd1fae2b5067
juliachruszczowa <julia.chruszczowa@datylon.svaps.com>
2022-02-16 19:39:18 +0300

    fix: remove unused code

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -49,26 +49,6 @@ function Footer() {
       </Menu.Item>
     </Menu>
   );
-
-  return (
-    <footer className="footer">
-      <div className="footer-box">
-        <div className="footer-box-item">
-          <a href="https://github.com/juliachruszczowaH/rslang">
-            <img className="footer-logo" src={github} alt="github-logo" />
-          </a>
-        </div>
-        <div className="footer-box-item">
-          <p className="item-styles"> RSS Q3-2021</p>
-        </div>
-        <div className="footer-box-item">
-          <a href="https://rs.school/js/">
-            <img className="footer-logo" src={rss} alt="rss-logo" />
-          </a>
-        </div>
-      </div>
-    </footer>
-  );
 }
 
 export default Footer;
