9b29e504529c083b9878137cbf8b3620970495d8
juliachruszczowa <julia.chruszczowa@datylon.svaps.com>
2022-02-16 19:30:03 +0300

    feat: add styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,19 +1,70 @@
 import './footer.css';
 import github from '../../assets/github.png';
 import rss from '../../assets/rss-logo-unit.svg';
+import { Label, Menu, Image } from 'semantic-ui-react';
 
 function Footer() {
+  return (
+    <Menu  fluid widths={3}>
+      <Menu.Item>
+        <Label
+          as="a"
+          href="https://github.com/juliachruszczowaH"
+          target="_blank"
+        >
+          <Image
+            avatar
+            spaced="right"
+            src="https://react.semantic-ui.com/images/avatar/small/joe.jpg"
+          />
+          juliachruszczowaH
+        </Label>
+
+        <Label as="a" href="https://github.com/nnadeysha" target="_blank">
+          <Image
+            avatar
+            spaced="right"
+            src="https://react.semantic-ui.com/images/avatar/small/stevie.jpg"
+          />
+          nnadeysha
+        </Label>
+        <Label as="a" href="https://github.com/alexboagreek/" target="_blank">
+          <Image
+            avatar
+            spaced="right"
+            src="https://react.semantic-ui.com/images/avatar/small/elliot.jpg"
+          />
+          alexboagreek
+        </Label>
+      </Menu.Item>
+      <Menu.Item> &copy; 2022</Menu.Item>
+
+      <Menu.Item>
+        <Image
+          href="https://rs.school/js/"
+          target="_blank"
+          size="tiny"
+          src={rss}
+        />
+      </Menu.Item>
+    </Menu>
+  );
+
   return (
     <footer className="footer">
       <div className="footer-box">
         <div className="footer-box-item">
-          <a href="https://github.com/juliachruszczowaH/rslang"><img className="footer-logo" src={ github } alt="github-logo" /></a>
+          <a href="https://github.com/juliachruszczowaH/rslang">
+            <img className="footer-logo" src={github} alt="github-logo" />
+          </a>
         </div>
         <div className="footer-box-item">
-           <p className="item-styles"> RSS Q3-2021</p>
+          <p className="item-styles"> RSS Q3-2021</p>
         </div>
         <div className="footer-box-item">
-          <a href="https://rs.school/js/"><img className="footer-logo" src={ rss } alt="rss-logo" /></a>
+          <a href="https://rs.school/js/">
+            <img className="footer-logo" src={rss} alt="rss-logo" />
+          </a>
         </div>
       </div>
     </footer>
--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -1,81 +1,202 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 import './home.css';
-import HomeCardItem from './HomeItem';
-import { Statistic } from 'semantic-ui-react';
+import {
+  Grid,
+  Label,
+  Image,
+  Card,
+  Transition,
+  Embed,
+  Container,
+  Header,
+  Rail,
+  Sticky,
+} from 'semantic-ui-react';
 import book from '../../assets/book.png';
 import dictionary from '../../assets/dictionary.png';
 import game from '../../assets/game.png';
 import stat from '../../assets/statistics.png';
+import { isAuthenticated } from '../../services/AuthService';
+import { LoginModal } from '../../components/LoginModal';
 
 const Home: React.FC = () => {
-  return (
-     
-    <div className="home-content">
-     
-      <div className="advantages">
-          <div className="home-content-box">
-            <h1 className="box-title">RS Lang</h1>
-            <p className="basic-text">
-              Learning English has never been so easy
-              Memorizing English words can be fun and challenging.
-              Play games, listen to pronunciation, improve your knowledge.
-              With our app, learning is a joy.
-            </p>
-          </div>
-          <h2 className="box-title">Advantages</h2>
-          <div className="advantages-box">
-          <div className="advantages-box-description">
-            <img className="icons" src={ book } alt="book-logo" />
-            <h2>Book</h2>
-              <p>
-                The electronic textbook consists of six sections.
-                Each section has 30 pages of 20 words. The translation of the word, the thematic image,
-                as well as the pronunciation of both the word separately and as part of the phrase are presented.
-              </p>
-            </div>
-            <div className="advantages-box-description">
-              <img className="icons" src= { dictionary } alt="dictionary-logo" />
-              <h2>Dictionary</h2>
-              <p>
-                The dictionary contains lists of studied words,
-                words that do not need to be learned, as well as those that cause difficulties.
-                The dictionary reflects statistics for each section and student progress.
-              </p>
-            </div>
-        </div>
-        <div className="advantages-box">
-          <div className="advantages-box-description">
-            <img className="icons" src= { game } alt="game" />
-                <h2>Games</h2>
-            <p>
-              For learning words and reinforcing memorization,
-              the application has 2 games: Sprint, Audio Challenge
-              which will help you to "pump" your vocabulary in a playful way.
-            </p>
+  const [auth, setAuth] = useState(isAuthenticated());
+  const [visibility, setVisibility] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
 
-          </div>
-          <div className="advantages-box-description">
-            <img className="icons" src={ stat } alt="statistic" />
-            <h2>Statistic</h2>
-            <p>
-              All the progress of training can be viewed in statistics,
-              where data for the current day, as well as for the entire training period, are presented.
-              The information is presented both in the form of a table and graphs, which is very convenient..
-            </p>
-          </div>
-        </div>
-      </div>
-      <div className="presentation">
-        <h2 className='box-title'>Video Presentation</h2>
-      </div>
+  useEffect(() => {
+    if (location.pathname === '/home' || location.pathname === '/') {
+      setVisibility(true);
+    }
+  }, [visibility, location.pathname]);
+
+  useEffect(() => {
+    setAuth(isAuthenticated());
+  }, [auth]);
+
+  return (
+    <div>
+      <Grid centered stackable relaxed>
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Container fluid textAlign="center">
+              <Header as="h1">RS Lang</Header>
+              <p style={{ fontSize: '18px' }}>
+                Learning English has never been so easy Memorizing English words
+                can be fun and challenging. Play games, listen to pronunciation,
+                improve your knowledge. With our app, learning is a joy.
+              </p>
+            </Container>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Transition
+              visible={visibility}
+              animation="fly down"
+              duration="1000"
+            >
+              <Card style={{ padding: '15px' }} centered>
+                <Image src={book} centered size="tiny" />
+                <Card.Content>
+                  <Label
+                    as="a"
+                    color="red"
+                    ribbon
+                    onClick={() => navigate('/book')}
+                  >
+                    Book
+                  </Label>
+                  <Card.Description>
+                    The electronic textbook consists of six sections. Each
+                    section has 30 pages of 20 words. The translation of the
+                    word, the thematic image, as well as the pronunciation of
+                    both the word separately and as part of the phrase are
+                    presented.
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Transition>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Transition
+              visible={visibility}
+              animation="fly left"
+              duration="1000"
+            >
+              <Card centered style={{ padding: '15px' }}>
+                <Image src={dictionary} centered size="tiny" />
+
+                <Card.Content>
+                  {!auth ? (
+                    LoginModal(
+                      <Label as="a" color="green" ribbon>
+                        Dictionary
+                      </Label>,
+                    )
+                  ) : (
+                    <Label
+                      as="a"
+                      color="green"
+                      ribbon
+                      onClick={() => navigate('/dictionary')}
+                    >
+                      Dictionary
+                    </Label>
+                  )}
+                  <Card.Description>
+                    The dictionary contains lists of studied words, words that
+                    do not need to be learned, as well as those that cause
+                    difficulties. The dictionary reflects statistics for each
+                    section and student progress.
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Transition>
+          </Grid.Column>
+        </Grid.Row>
+
+        <Grid.Row>
+          <Grid.Column width={6}>
+            <Transition
+              visible={visibility}
+              animation="fly right"
+              duration="1000"
+            >
+              <Card centered style={{ padding: '15px' }}>
+                <Image src={game} centered size="tiny" />
+                <Card.Content>
+                  <Label
+                    as="a"
+                    color="blue"
+                    ribbon
+                    onClick={() => navigate('/sprintgame')}
+                  >
+                    Sprint
+                  </Label>
+                  <Card.Description>
+                    <Label
+                      as="a"
+                      color="teal"
+                      ribbon
+                      onClick={() => navigate('/audiocall')}
+                    >
+                      Audiocall
+                    </Label>
+                    Learning English has never been so easy Memorizing English
+                    words can be fun and challenging. Play games, listen to
+                    pronunciation, improve your knowledge. With our app,
+                    learning is a joy.
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Transition>
+          </Grid.Column>
+          <Grid.Column width={6}>
+            <Transition visible={visibility} animation="fly up" duration="1000">
+              <Card centered style={{ padding: '15px' }}>
+                <Image src={stat} centered size="tiny" />
+                <Card.Content>
+                  {!auth ? (
+                    LoginModal(
+                      <Label as="a" color="orange" ribbon>
+                        Statistic
+                      </Label>,
+                    )
+                  ) : (
+                    <Label
+                      as="a"
+                      color="orange"
+                      ribbon
+                      onClick={() => navigate('/statistics')}
+                    >
+                      Statistic
+                    </Label>
+                  )}
+                  <Card.Description>
+                    All the progress of training can be viewed in statistics,
+                    where data for the current day, as well as for the entire
+                    training period, are presented. The information is presented
+                    both in the form of a table and graphs, which is very
+                    convenient..
+                  </Card.Description>
+                </Card.Content>
+              </Card>
+            </Transition>
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row>
+          <Grid.Column width={10}>
+            <Transition visible={visibility} animation="fade" duration="2500">
+              <Embed placeholder="/images/image-16by9.png" source="youtube" />
+            </Transition>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
     </div>
   );
 };
 
 export default Home;
-
-
-
-
-
-
